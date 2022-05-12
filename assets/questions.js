dc102d9675a986a2df4c0d4514ef940dc899edf7
drewmckinney23 <drewmckinney23@gmail.com>
2022-05-12 16:18:47 -0400

    added questions.js and script.js

---
+++ assets/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the default behavior called that is used to move declarations to the top of the current scope?",
+    answer: "String",
+    options: [
+      "Jumping",
+      "Arranging",
+      "Sorting",
+      "Hoisting"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+    answer: "Loop",
+    options: [
+      "Repeater",
+      "Loop",
+      "Debugger",
+      "Clone"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the name of the object that allows you to perform mathematical tasks with the interpreter?",
+    answer: "Math",
+    options: [
+      "Number",
+      "Solve",
+      "Count",
+      "Math"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What kind of statement is used to execute actions based on a trigger or condition?",
+    answer: "Conditional Statement",
+    options: [
+      "Fired Event",
+      "Conditional Statement",
+      "Regular Expression",
+      "Boolean Variable"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+    answer: "Boolean",
+    options: [
+      "Boolean",
+      "RegExp",
+      "Condition",
+      "EVent"
+    ]
+  },
+  
+];
